17df5bd615f6f6fd3c9304ea8e5487999ca0d130
WSVrushali <48611727+WSVrushali@users.noreply.github.com>
2020-11-21 13:18:17 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld{
 	public static void main(String args[]){
-		System.out.println("Welcome to Jenkins using GitHub !!!");	
+		System.out.println("Welcome to Jenkins using Git and GitHub !!!");	
 	}
 }
